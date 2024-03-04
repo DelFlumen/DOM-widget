874da35a0f133dc9fe2c86cc1a6844294a803ac1
Yaroslav Zaychenko <delflumen@ukr.net>
2024-03-04 16:56:10 +0200

    react app setup

--- dom-widget/src/App.tsx
+++ dom-widget/src/App.tsx
@@ -3,22 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // console.log(window.parent.document);
+  window.parent.postMessage({message: 'Hello world'}, "*");
+
+  // window.addEventListener('message', function(event) {
+  //   // if(event.origin === 'http://localhost/3000')
+  //   // {
+  //     // alert('Received message: ' + event.data.message);
+  //     console.log(event.data)
+  //   // }
+  //   // else
+  //   // {
+  //   //   alert('Origin not allowed!');
+  //   // }
+  // }, false);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>DOM tree widget</h1>
     </div>
   );
 }
