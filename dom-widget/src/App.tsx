7e5442fffdcc284085fc9866f1a80ea842eb1b7d
Yaroslav Zaychenko <delflumen@ukr.net>
2024-03-05 20:19:37 +0200

    fix page reload bug and add styling

--- dom-widget/src/App.tsx
+++ dom-widget/src/App.tsx
@@ -1,34 +1,52 @@
 import {useEffect, useState, useCallback} from 'react';
 import './App.css';
 
-type Node = { tag: string; attributes: NamedNodeMap; children: Node[] | null; } | null;
+type Node = { tag: string; id?: string; className?: string; children: Node[] | null; } | null;
 
 function App() {
   const [tree, setTree] = useState<Node>(null);
-  window.parent.postMessage({message: 'Hello world'}, "*");
 
-  const handleGetDOM = useCallback((event: MessageEvent<any>) => {
-    const generateDOMTree = (node: HTMLElement | Element | null) => {
-      if (!node) return null;
-      const children: Node[] = Array.from(node.children).map((child) => generateDOMTree(child));
+console.log({tree})
 
-      return {tag: node.tagName, attributes: node.attributes, children};
-    }
+  useEffect(() => {
+    window.parent.postMessage({type: 'getDOM'}, '*');
+  
+    
+  }, [])
+  
+  const generateDOMTree = (node: HTMLElement | Element | null) => {
+    if (!node) return null;
+    const children: Node[] = Array.from(node.children).map((child) => generateDOMTree(child));
+
+    // @ts-expect-error
+    const id = node?.attributes && node.attributes.id ? node.attributes.value : null;
+    // @ts-expect-error
+    const className = node?.attributes && node.attributes.class ? node.attributes.class.value : null;
+
+    return {tag: node.tagName, id, className, children};
+  }
+
+  const handleGetDOM = (event: MessageEvent<any>) => {
 
     const parser = new DOMParser();
     const doc = parser.parseFromString(event.data.parentDocument, 'text/html');
     console.log({doc})
+    if (!doc.querySelector('body')?.children.length) return;
 
     //remove iframeElem from parent html copy
     const iframeElem = doc.querySelector('iframe');
     if (iframeElem) {
       iframeElem.parentNode?.removeChild(iframeElem);
     }
+    const scriptElem = doc.querySelector('script');
+    if (scriptElem) {
+      scriptElem.parentNode?.removeChild(scriptElem);
+    }
 
     const generatedTree = generateDOMTree(doc.querySelector('body'));
 
     setTree(generatedTree);    
-  }, []);
+  };
 
   useEffect(() => {
     window.addEventListener('message', handleGetDOM, false);
@@ -37,6 +55,7 @@ function App() {
       window.removeEventListener('message', handleGetDOM, false);
     }
   }, []);
+
 console.log(tree);
 
   // window.addEventListener('message', function(event) {
@@ -51,25 +70,35 @@ console.log(tree);
   //   // }
   // }, false);
 
-  const highlightNode = (node: Node) => {
-    window.parent.postMessage({type: 'highlightNode', tagName: node?.tag, attributes: node?.attributes}, '*');
+  const highlightNode = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>, node: Node) => {
+    console.log('hover');
+    e.stopPropagation();
+    window.parent.postMessage({type: 'highlightNode', tag: node?.tag, id: node?.id, className: node?.className }, '*');
   }
 
-  const renderParentDOM = (tree: Node) => { 
+  const undoHighlightNode = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>, node: Node) => {
+    console.log('hover');
+    e.stopPropagation();
+  
+
+    window.parent.postMessage({type: 'undoHighlightNode', tag: node?.tag, id: node?.id, className: node?.className }, '*');
+  }
+
+  const renderParentDOM = (tree: Node, level = 0, idx?: number) => { 
     if (!tree) return null;
 
     return (
-    <ul>
-      <li key={tree.tag} onMouseEnter={() => highlightNode(tree)}>
-        {tree.tag}
-        {tree.children && tree.children.map(child => renderParentDOM(child))}
+    <ul key={`${tree.tag}-${tree.id}-${tree.className}-${level}-${idx}-ul`}>
+      <li key={`${tree.tag}-${tree.id}-${tree.className}-${level}-${idx}-li`}>
+        <div onMouseEnter={(e) => highlightNode(e, tree)} onMouseLeave={(e) => undoHighlightNode(e, tree)}>{tree.tag}</div>
+        {tree.children && tree.children.map((child, idx) => renderParentDOM(child, level + 1, idx))}
       </li> 
     </ul>)
    }
   
   return (
     <div className="App">
-      <h1>DOM tree widget</h1>
+      <h1>DOM tree</h1>
       {tree && renderParentDOM(tree)}
     </div>
   );
